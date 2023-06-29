9a11a4f7bb8a34b91549f5b0067030dcd0891c14
mariademelo <mdemelo@algomau.ca>
2023-06-28 22:42:48 -0400

    set up to scrape every 20 hours

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
+
 from bs4 import BeautifulSoup
 import csv
 import requests
+import time
 
-website = requests.get('https://www.honda.com.br/motos/modelos').text
+def scrapper():
+    website = requests.get('https://www.honda.com.br/motos/modelos').text
+    soup = BeautifulSoup(website, 'html.parser')
+    items = soup.findAll('li', class_='item')
 
-soup = BeautifulSoup(website, 'html.parser')
+    filename = "output.csv"
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Model", "Price"])  # Write headers
 
-items = soup.findAll('li', class_='item')
+        for item in items:
+            name = item.find('div', class_='title').text.replace("\n", "").replace(" ", "")
+            price = item.find('span', class_='price').text.replace(" ", "")
+            print(f"Model: {name:35} Price: {price}")
+            writer.writerow([name, price])
 
-filename = "output.csv"
-with open(filename, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["Model", "Price"])  # Write headers
+        print(f"File updated.")
 
-    for item in items:
-        name = item.find('div', class_='title').text.replace("\n", "").replace(" ", "")
-        price = item.find('span', class_='price').text.replace(" ", "")
-        print(f"Model: {name:35} Price: {price}")
-        writer.writerow([name, price])
-
-print(f"File updated.")
+if __name__ == '__main__':
+   #run every 20 hours
+    while True:
+        scrapper()
+        time.sleep(600*60*2)
