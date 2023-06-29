d99754129159aa17859c1d09f2cabb9ad41b16c4
mariademelo <mdemelo@algomau.ca>
2023-06-28 22:14:00 -0400

    Printed all models/prices

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ website = requests.get('https://www.honda.com.br/motos/modelos').text
 
 soup = BeautifulSoup(website, 'html.parser')
 
+items = soup.findAll('li', class_='item')
 
-name = soup.find('div', class_ = 'title').text
-print(name)
-price = soup.find('span', class_ = 'price').text
-print(price)
+for item in items:
+    name = item.find('div', class_ = 'title').text.replace("\n", "").replace(" ","")
+    price = item.find('span', class_ = 'price').text.replace(" ", "")
+    print(f"Model: {name:35} Price: {price}")
